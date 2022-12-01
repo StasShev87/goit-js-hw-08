ef55030bf321738f6c31f09aaf508cc77cc7676c
Stanislav Shevchenko <StasShev87@gmail.com>
2022-12-02 01:17:54 +0200

    Add feedback form load and submit behaviour

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const localStorageKey = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const formEmail = document.querySelector('.feedback-form > input[name=email]');
+const formMessage = document.querySelector(
+  '.feedback-form > input[name=message]'
+);
+
+// Restore form fields from local storage
+try {
+  const formDataJSON = localStorage.getItem(localStorageKey);
+  if (formDataJSON !== null) {
+    const formData = JSON.parse(formDataJSON);
+    formEmail.value = formData.email || '';
+    formMessage.value = formData.message || '';
+  }
+} catch (error) {
+  console.error('Get state error: ', error.message);
+  localStorage.removeItem(localStorageKey);
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const { email, message } = event.currentTarget.elements;
+
+  //   if (email.value === '' || message.value === '') {
+  //     return alert('Please fill in all the fields!');
+  //   }
+
+  const formData = { email: email.value, message: message.value };
+
+  console.log(formData);
+  event.currentTarget.reset();
+
+  try {
+    localStorage.removeItem(localStorageKey);
+  } catch (error) {
+    console.error('Remove state error: ', error.message);
+  }
+});
