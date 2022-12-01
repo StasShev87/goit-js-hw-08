1fd664f979e694bf1c3393505a3fcaed051ac380
Stanislav Shevchenko <StasShev87@gmail.com>
2022-12-02 00:08:59 +0200

    Add task 2 "video player" playback backup to local storage

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import VimeoPlayer from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new VimeoPlayer(iframe);
+
+const localStorageKey = 'videoplayer-current-time';
+// TODO:Restore current playback position
+const positionJSON = localStorage.getItem(localStorageKey);
+if (positionJSON) {
+  try {
+    const position = JSON.parse(positionJSON);
+    player.setCurrentTime(position.seconds);
+  } catch {
+    localStorage.removeItem(localStorageKey);
+  }
+}
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.on('timeupdate', function (currentPlaybackPosition) {
+  console.log(currentPlaybackPosition);
+  // Backup current playback posiotion
+  const positionJSON = JSON.stringify(currentPlaybackPosition);
+  localStorage.setItem(localStorageKey, positionJSON);
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
