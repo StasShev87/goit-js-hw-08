c278a558f0191a69df2778acdaaa1b07ae83d616
Stanislav Shevchenko <StasShev87@gmail.com>
2022-12-07 00:37:49 +0200

    Fix feedback message query selector, remove try catch

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,40 +5,31 @@ const localStorageKey = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
 const formEmail = document.querySelector('.feedback-form input[name=email]');
 const formMessage = document.querySelector(
-  '.feedback-form input[name=message]'
+  '.feedback-form textarea[name=message]'
 );
 
 // Restore form fields from local storage
-try {
-  const formDataJSON = localStorage.getItem(localStorageKey);
-  if (formDataJSON !== null) {
-    const formData = JSON.parse(formDataJSON);
-    formEmail.value = formData.email || '';
-    formMessage.value = formData.message || '';
-  }
-} catch (error) {
-  console.error('Get state error: ', error.message);
-  localStorage.removeItem(localStorageKey);
+const formDataJSON = localStorage.getItem(localStorageKey);
+if (formDataJSON !== null) {
+  const formData = JSON.parse(formDataJSON);
+  formEmail.value = formData.email || '';
+  formMessage.value = formData.message || '';
 }
 
 form.addEventListener('submit', event => {
   event.preventDefault();
   const { email, message } = event.currentTarget.elements;
 
-  //   if (email.value === '' || message.value === '') {
-  //     return alert('Please fill in all the fields!');
-  //   }
+  if (email.value === '' || message.value === '') {
+    return alert('Please fill in all the fields!');
+  }
 
   const formData = { email: email.value, message: message.value };
 
   console.log(formData);
   event.currentTarget.reset();
 
-  try {
-    localStorage.removeItem(localStorageKey);
-  } catch (error) {
-    console.error('Remove state error: ', error.message);
-  }
+  localStorage.removeItem(localStorageKey);
 });
 
 form.addEventListener(
@@ -47,12 +38,9 @@ form.addEventListener(
     const { email, message } = event.currentTarget.elements;
     const formData = { email: email.value, message: message.value };
     console.log(formData);
+
     // Backup form data
-    try {
-      const formDataJSON = JSON.stringify(formData);
-      localStorage.setItem(localStorageKey, formDataJSON);
-    } catch (error) {
-      console.error('Set state error: ', error.message);
-    }
+    const formDataJSON = JSON.stringify(formData);
+    localStorage.setItem(localStorageKey, formDataJSON);
   }, 500)
 );
